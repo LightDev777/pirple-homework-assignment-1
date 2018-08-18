2840e0cb84eca5edb869350a289cc3b99f598f1d
Charmie Quino <charmiequino@gmail.com>
2018-08-19 01:31:20 +0800

    work progress and assignment

---
+++ config.js
@@ -0,0 +1,33 @@
+/* Create and export environment variables
+*
+*/
+
+// general container for all the environments
+
+var environments = {};
+
+// Staging (default) environment
+environments.staging = {
+  'httpPort' : 3000,
+  'httpsPort' : 3001,
+  'envName' : 'staging'
+};
+
+
+// Production environment
+environments.production = {
+  'httpPort' : 5000,
+  'httpsPort': 5001,
+  'envName' : 'production'
+};
+
+
+// Determine which environment to be used
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+
+// Check that the current environment is one of the environments above, if not, default to staginig 
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// export module
+module.exports = environmentToExport;
---
+++ index.js
@@ -0,0 +1,133 @@
+/*
+* Primary file for the API
+* Author: Charmie Q
+*
+*/
+
+// Dependencies
+var http = require('http'),
+    https = require('https'),
+    url = require('url'),
+    StringDecoder = require('string_decoder').StringDecoder,
+    config = require('./config'),
+    fs = require('fs');
+
+// Instantiate the HTTP server
+var httpServer = http.createServer(function(req, res) {
+  unifiedServer(req, res);
+});
+
+// start the http server
+httpServer.listen(config.httpPort, function() {
+  console.log("The server is listening to port " + config.httpPort);
+});
+
+// Instantiate the HTTPS server
+var httpsServerOptions = {
+  'key' : fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
+  unifiedServer(req, res);
+});
+
+// start the https server
+httpsServer.listen(config.httpsPort, function() {
+  console.log("The server is listening to port " + config.httpsPort);
+});
+
+// Unified server to handle http and https
+
+var unifiedServer = function(req, res) {
+
+  // get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // get the query string as an object
+  var queryStringObject = parsedUrl.query;
+
+  // get the http method
+  var method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  var headers = req.headers;
+
+  // get the payload, if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function() {
+    buffer += decoder.end();
+
+    // choose the handler this request should go to. If none is found then use notfound handler
+    var chooseHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Construct the data object to send to the handler
+
+    var data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
+
+    // Route the request to the handler specified in the router
+    chooseHandler(data, function(statusCode, payload) {
+      // Use the status code called back by the handler or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+      // Use the payload called back by the handler or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert payload to a string
+      var payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Log the return response
+      console.log('Return response', statusCode, payloadString);
+    });
+  });
+}
+
+// Define the handlers
+var handlers = {};
+
+// ping handler
+handlers.ping = function(data, callback) {
+  callback(200, message);
+};
+
+// handler to send a 'hello world' message
+handlers.sayhi = function(data, callback) {
+  var message = {
+    'message' : 'Hello World!'
+  };
+  callback(200, message);
+};
+
+// not found handler
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+
+// Define a request router
+var router = {
+  'ping' : handlers.ping,
+  'hello': handlers.sayhi
+};
+
+
